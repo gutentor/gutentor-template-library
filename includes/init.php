9d39f80637aa860c97466de2d4d15868cc5a84ad
aneelmaharjan <anil@acmethemes.com>
2020-06-10 10:58:03 +0545

    commented newly added block

--- includes/init.php
+++ includes/init.php
@@ -208,7 +208,7 @@ class Gutentor_Template_Library {
 		/*Modules*/
 		require_once GUTENTOR_TEMPLATE_LIBRARY_PATH . 'includes/template-add/module/class-post-module.php';
 		require_once GUTENTOR_TEMPLATE_LIBRARY_PATH . 'includes/template-add/module/class-post-feature.php';
-		require_once GUTENTOR_TEMPLATE_LIBRARY_PATH . 'includes/template-add/module/class-advanced-post-module.php';
+		//require_once GUTENTOR_TEMPLATE_LIBRARY_PATH . 'includes/template-add/module/class-advanced-post-module.php';
 
 		$this->loader = new Gutentor_Template_Library_Loader();
 	}
