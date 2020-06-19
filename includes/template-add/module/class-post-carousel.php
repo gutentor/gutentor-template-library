162816b607cdac72b791abc6dcc9250434f086af
aneelmaharjan <anil@acmethemes.com>
2020-06-19 16:37:21 +0545

    Added: New blocks like Advanced Post Module, Post Carousel, News Ticker

---
+++ includes/template-add/module/class-post-carousel.php
@@ -0,0 +1,102 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Gutentor_Template_Library_Block_Post_Carousel' ) ) {
+
+	/**
+	 * Functions related to Video_Popup
+	 * @package Gutentor
+	 * @since 1.0.1
+	 *
+	 */
+
+	class Gutentor_Template_Library_Block_Post_Carousel extends Gutentor_Template_Library_Base{
+
+		/**
+		 * Name of the block.
+		 *
+		 * @access protected
+		 * @since 1.0.1
+		 * @var string
+		 */
+		protected $block_name = 'p1';
+
+		/**
+		 * Gets an instance of this object.
+		 * Prevents duplicate instances which avoid artefacts and improves performance.
+		 *
+		 * @static
+		 * @access public
+		 * @since 1.0.1
+		 * @return object
+		 */
+		public static function get_instance() {
+
+			// Store the instance locally to avoid private static replication
+			static $instance = null;
+
+			// Only run these methods if they haven't been ran previously
+			if ( null === $instance ) {
+				$instance = new self();
+			}
+
+			// Always return the instance
+			return $instance;
+
+		}
+
+		/**
+         * Load block library
+         * Used for blog template loading
+         *
+         * @since      1.1.5
+         * @package    Gutentor
+         * @author     Gutentor <info@gutentor.com>
+         *
+         * @param $templates_list array
+         * @return array
+         */
+        public function add_block_template_library( $templates_list ){
+
+            $block_library_list = array(
+
+                array(
+					'title'				=> __( 'Post Carousel', 'gutentor' ),
+					'type'				=> 'block',
+					'author'			=> __( 'Gutentor', 'gutentor' ),
+					'keywords'			=> array( 'post-carousel', 'post-carousel 1' ),
+					'categories'		=> array( 'post-carousel' ),
+					'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_URL.'includes/template-data/module/post-carousel/template-1/gutentor_p3.json',
+					'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_URL.'includes/template-data/module/post-carousel/template-1/template-1.jpg',
+					'demo_url'    => 'https://www.demo.gutentor.com/free-block-demo/post-carousel/#6d2f0a30-c266-4cb6-8663-1b718547277d',
+				),
+				array(
+					'title'				=> __( 'Post Carousel', 'gutentor' ),
+					'type'				=> 'block',
+					'author'			=> __( 'Gutentor', 'gutentor' ),
+					'keywords'			=> array( 'post-carousel', 'post-carousel 2' ),
+					'categories'		=> array( 'post-carousel' ),
+					'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_URL.'includes/template-data/module/post-carousel/template-2/gutentor_p3.json',
+					'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_URL.'includes/template-data/module/post-carousel/template-2/template-2.jpg',
+					'demo_url'    => 'https://www.demo.gutentor.com/free-block-demo/post-carousel/#gmaba16f4',
+				),
+				array(
+					'title'				=> __( 'Post Carousel', 'gutentor' ),
+					'type'				=> 'block',
+					'author'			=> __( 'Gutentor', 'gutentor' ),
+					'keywords'			=> array( 'post-carousel', 'post-carousel 3' ),
+					'categories'		=> array( 'post-carousel' ),
+					'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_URL.'includes/template-data/module/post-carousel/template-3/gutentor_p3.json',
+					'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_URL.'includes/template-data/module/post-carousel/template-3/template-3.jpg',
+					'demo_url'    => 'https://www.demo.gutentor.com/free-block-demo/post-carousel/#gm8199b1a',
+				),
+                
+            );
+
+            return array_merge_recursive( $templates_list, $block_library_list );
+        }
+	}
+}
+Gutentor_Template_Library_Block_Post_Carousel::get_instance()->run();
