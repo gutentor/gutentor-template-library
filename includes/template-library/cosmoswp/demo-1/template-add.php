eeaf10029e438ca08232b1ee5d03462ff94a5146
aneelmaharjan <anil@acmethemes.com>
2020-07-13 17:38:43 +0545

    Added: CosmosWP Demo 1 Home Page Template

--- includes/template-library/cosmoswp/demo-1/template-add.php
+++ includes/template-library/cosmoswp/demo-1/template-add.php
@@ -53,25 +53,36 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_1' ) ) {
 
             $block_library_list = array(
 
+                // array(
+                //     'title'				=> __( 'About', 'gutentor' ),
+                //     'type'				=> 'block',
+                //     'author'			=> __( 'CosmosWP', 'gutentor' ),
+                //     'keywords'			=> array( 'widget', 'about 1' ),
+                //     'categories'		=> array( 'cosmoswp','widget','about' ),
+                //     'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/template.json',
+                //     'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/screenshot.jpg',
+                //     'demo_url'    => 'https://www.demo.gutentor.com/free-block-demo/about/#section-1699fc93-7cc0-4c85-a359-39d052779978',
+                // ),
+                // array(
+                //     'title'				=> __( 'About Block', 'gutentor' ),
+                //     'type'				=> 'block',
+                //     'author'			=> __( 'Gutentor', 'gutentor' ),
+                //     'keywords'			=> array( 'about-block', 'about 1' ),
+                //     'categories'		=> array( 'about' ),
+                //     'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/template.json',
+                //     'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/screenshot.jpg',
+                //     'demo_url'    => 'https://www.demo.gutentor.com/free-block-demo/about/#section-1699fc93-7cc0-4c85-a359-39d052779978',
+                // ),
+                
                 array(
-                    'title'				=> __( 'About', 'gutentor' ),
-                    'type'				=> 'block',
-                    'author'			=> __( 'CosmosWP', 'gutentor' ),
-                    'keywords'			=> array( 'widget', 'about 1' ),
-                    'categories'		=> array( 'cosmoswp','widget','about' ),
-                    'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/template.json',
-                    'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/screenshot.jpg',
-                    'demo_url'    => 'https://www.demo.gutentor.com/free-block-demo/about/#section-1699fc93-7cc0-4c85-a359-39d052779978',
-                ),
-                array(
-                    'title'				=> __( 'About Block', 'gutentor' ),
-                    'type'				=> 'block',
-                    'author'			=> __( 'Gutentor', 'gutentor' ),
-                    'keywords'			=> array( 'about-block', 'about 1' ),
-                    'categories'		=> array( 'about' ),
-                    'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/template.json',
-                    'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/modules/about/screenshot.jpg',
-                    'demo_url'    => 'https://www.demo.gutentor.com/free-block-demo/about/#section-1699fc93-7cc0-4c85-a359-39d052779978',
+                    'title'				=> __( 'Home', 'gutentor' ),
+                    'type'				=> 'template',
+                    'author'			=> __( 'CosmosWP', 'cosmmoswp' ),
+                    'keywords'			=> array( 'home-page', 'home page' ),
+                    'categories'		=> array( 'home' ),
+                    'template_url'		=> GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/templates/home/template.json',
+                    'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL.'demo-1/template-data/templates/home/screenshot.jpg',
+                    'demo_url'    => 'https://www.demo.cosmoswp.com/demo-1/',
                 ),
             );
 
