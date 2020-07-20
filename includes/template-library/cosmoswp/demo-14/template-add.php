b542f197ef7ae32f19bef24cbf506441ca95fcea
aneelmaharjan <anil@acmethemes.com>
2020-07-20 15:58:09 +0545

    Added: CosmosWP Demo 14 --> Home & Advance Column Modules Templates

---
+++ includes/template-library/cosmoswp/demo-14/template-add.php
@@ -0,0 +1,92 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_14' ) ) {
+
+    /**
+     * Functions related to Demo 14
+     * @package Gutentor
+     * @since 1.1.0
+     *
+     */
+
+    class Gutentor_Template_Library_CosmosWP_Demo_14 extends Gutentor_Template_Library_Base{
+
+        /**
+         * Gets an instance of this object.
+         * Prevents duplicate instances which avoid artefacts and improves performance.
+         *
+         * @static
+         * @access public
+         * @since 1.1.0
+         * @return object
+         */
+        public static function get_instance() {
+
+            // Store the instance locally to avoid private static replication
+            static $instance = null;
+
+            // Only run these methods if they haven't been ran previously
+            if ( null === $instance ) {
+                $instance = new self();
+            }
+
+            // Always return the instance
+            return $instance;
+
+        }
+
+        /**
+         * Load block library
+         * Used for blog template loading
+         *
+         * @since      1.1.5
+         * @package    Gutentor
+         * @author     Gutentor <info@gutentor.com>
+         *
+         * @param $templates_list array
+         * @return array
+         */
+        public function add_block_template_library( $templates_list ){
+
+            $block_library_list = array(
+
+                //Block/Modules Templates Starts
+
+                array(
+                    'title'                => __('Advanced Columns', 'gutentor'),
+                    'type'                => 'block',
+                    'author'            => __('CosmosWP', 'gutentor'),
+                    'keywords'            => array('advanced-columns', 'advanced columns 1'),
+                    'categories'        => array('cosmoswp', 'advanced-columns'),
+                    'template_url'        => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL . 'demo-14/template-data/modules/advance-column/advance-column-1/template.json',
+                    'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL . 'demo-14/template-data/modules/advance-column/advance-column-1/screenshot.jpg',
+                    'demo_url'    => 'https://www.demo.cosmoswp.com/demo-14/#home',
+                ),
+               
+
+                //Block Templates ends
+
+
+                //Page Templates Starts
+                array(
+                    'title'             => __('Home 14', 'gutentor' ),
+                    'type'              => 'template',
+                    'author'            => __( 'CosmosWP', 'gutentor' ),
+                    'keywords'          => array('restaurant','home','home 14' ),
+                    'categories'        => array( 'cosmoswp', 'restaurant' ),
+                    'template_url'        => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL . 'demo-14/template-data/templates/home/template.json',
+                    'screenshot_url'    => GUTENTOR_TEMPLATE_LIBRARY_COSMOSWP_URL . 'demo-14/template-data/templates/home/screenshot.jpg',
+                    'demo_url'    => 'https://www.demo.cosmoswp.com/demo-14/',
+                ),
+               
+                //Page Templates Ends
+            );
+
+            return array_merge_recursive( $templates_list, $block_library_list );
+        }
+    }
+}
+Gutentor_Template_Library_CosmosWP_Demo_14::get_instance()->run();
