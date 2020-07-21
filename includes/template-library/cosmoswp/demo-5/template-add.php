804c58fdd41ff37db4e852008783ea0d2f80ad95
aneelmaharjan <anil@acmethemes.com>
2020-07-21 17:02:08 +0545

    Updated: CosmosWP Demo 5 -> Type block changed to widgets

--- includes/template-library/cosmoswp/demo-5/template-add.php
+++ includes/template-library/cosmoswp/demo-5/template-add.php
@@ -56,7 +56,7 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_5' ) ) {
                 //Block/Modules Templates Starts
                 array(
 					'title'				=> __( 'Advanced Columns', 'gutentor' ),
-					'type'				=> 'block',
+					'type'				=> 'widget',
 					'author'			=> __( 'CosmosWP', 'gutentor' ),
 					'keywords'			=> array( 'advanced-columns', 'advanced columns' ),
 					'categories'		=> array( 'cosmoswp','advanced-columns' ),
@@ -66,7 +66,7 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_5' ) ) {
 				),
                 array(
 					'title'				=> __( 'Advanced Columns', 'gutentor' ),
-					'type'				=> 'block',
+					'type'				=> 'widget',
 					'author'			=> __( 'CosmosWP', 'gutentor' ),
 					'keywords'			=> array( 'advanced-columns', 'advanced columns' ),
 					'categories'		=> array( 'cosmoswp','advanced-columns' ),
@@ -77,7 +77,7 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_5' ) ) {
             
                 array(
 					'title'				=> __( 'Icon Box', 'gutentor' ),
-					'type'				=> 'block',
+					'type'				=> 'widget',
 					'author'			=> __( 'CosmosWP', 'gutentor' ),
 					'keywords'			=> array( 'icon-box', ),
 					'categories'		=> array( 'cosmoswp','icon-box' ),
@@ -87,7 +87,7 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_5' ) ) {
                 ),
                 array(
 					'title'				=> __( 'Team', 'gutentor' ),
-					'type'				=> 'block',
+					'type'				=> 'widget',
 					'author'			=> __( 'CosmosWP', 'gutentor' ),
 					'keywords'			=> array( 'team' ),
 					'categories'		=> array( 'cosmoswp','team' ),
