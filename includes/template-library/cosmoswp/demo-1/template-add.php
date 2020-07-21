8528f3f008bd33740e42de976fcc39ebf267bd19
gutentor <gutentor@gmail.com>
2020-07-21 16:26:38 +0545

    widget type added

--- includes/template-library/cosmoswp/demo-1/template-add.php
+++ includes/template-library/cosmoswp/demo-1/template-add.php
@@ -56,7 +56,7 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_1' ) ) {
                 //Block/Modules Templates Starts
                 array(
 					'title'				=> __( 'About Block', 'gutentor' ),
-					'type'				=> 'block',
+					'type'				=> 'widget',
 					'author'			=> __( 'CosmosWP', 'gutentor' ),
 					'keywords'			=> array( 'about-block', 'about 3' ),
 					'categories'		=> array( 'cosmoswp','about' ),
