f29cad95c9d27006dc157c80ce462797da83fc95
aneelmaharjan <anil@acmethemes.com>
2020-07-21 17:05:28 +0545

    Updated: CosmosWP Demo 7 -> Type block changed to widgets

--- includes/template-library/cosmoswp/demo-7/template-add.php
+++ includes/template-library/cosmoswp/demo-7/template-add.php
@@ -56,7 +56,7 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_7' ) ) {
                 //Block/Modules Templates Starts
                 array(
 					'title'				=> __( 'Advanced Columns', 'gutentor' ),
-					'type'				=> 'block',
+					'type'				=> 'widget',
 					'author'			=> __( 'CosmosWP', 'gutentor' ),
 					'keywords'			=> array( 'advanced-columns', 'advanced columns' ),
 					'categories'		=> array( 'cosmoswp','advanced-columns' ),
@@ -66,7 +66,7 @@ if ( ! class_exists( 'Gutentor_Template_Library_CosmosWP_Demo_7' ) ) {
 				),
                 array(
 					'title'				=> __( 'Advanced Columns', 'gutentor' ),
-					'type'				=> 'block',
+					'type'				=> 'widget',
 					'author'			=> __( 'CosmosWP', 'gutentor' ),
 					'keywords'			=> array( 'advanced-columns', 'advanced columns' ),
 					'categories'		=> array( 'cosmoswp','advanced-columns' ),
